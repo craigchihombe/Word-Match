fda62e243bb4775e23104d54ed44cc0195a07a74
craigchihombe <achihombe@gmail.com>
2024-09-15 10:45:30 +0200

    Add files via upload

---
+++ Word match.py
@@ -0,0 +1,13 @@
+def match_word(words):
+    ctr = 0
+    lst = []
+    for word in words:
+        if len(word) > 1 and word[0]==word[-1]:
+            ctr +=1
+            list.append(word)
+            
+    print("List of words with the first and last character same \n",lst)
+    return ctr
+count = match_words(['abc', 'cfc','xyz', 'aba', '1221'])
+
+print("Number of words having first and last character same:", count)
